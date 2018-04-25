45a9228071b7062c9d9cd66ca937f413b1e7f832
Scott Weinstein <sweinstein27@gmail.com>
2018-04-25 17:34:38 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    new_last_array = "and #{array[-1]}"
+    array.pop
+    array.push(new_last_array)
+    return array.join(", ")
+  end
 end
