dbe11198ac20d6bed9726ac1f94e1019082c7e6c
Scott Weinstein <sweinstein27@gmail.com>
2018-04-25 17:30:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-array.join(" , ")
+  if array.length == 1
+    return "#{array[0]}" 
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    new_last_array = "and #{array[-1]}"
+    array.pop
+    array.push(new_last_array)
+    return array.join(", ")
 end
